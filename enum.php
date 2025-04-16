14e563d6ed988bad837802f7fb2aba4fc6055cf3
alyouma <alyoumaakmal4@gmail.com>
2025-04-16 07:53:23 +0700

    use enum

--- enum.php
+++ enum.php
@@ -18,4 +18,29 @@ class Customer
         $this->name = $name;
         $this->gender = $gender;
     }
+
+    public function __toString()
+    {
+        if ($this->gender == Gender::Male) {
+            return "Hello Mr. {$this->name}" . PHP_EOL;
+        } elseif ($this->gender == Gender::Female) {
+            return "Hello Mrs. {$this->name}";
+        } else {
+            return "Hello {$this->name}";
+        }
+    }
 }
+
+
+
+
+
+
+$customer = new Customer('Budi', Gender::Male);
+$customer2 = new Customer("Sarah", Gender::Female);
+
+
+var_dump((string)$customer);
+var_dump((string) $customer2);
+var_dump(Gender::cases()); // arry
+var_dump(Gender::Male);
